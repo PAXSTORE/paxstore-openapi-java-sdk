d7c698824318f16bfe4d5d8fd568ab762c210b8e
tanjie <tqjy13@163.com>
2019-03-11 18:01:47 +0800

    update test case

--- src/test/java/com/pax/market/api/sdk/java/api/test/TestConstants.java
+++ src/test/java/com/pax/market/api/sdk/java/api/test/TestConstants.java
@@ -18,13 +18,8 @@ package com.pax.market.api.sdk.java.api.test;
  */
 public interface TestConstants {
 	
-//	public static final String API_BASE_URL = "https://api.whatspos.com/p-market-api";
-//	public static final String API_KEY = "NGUIWQGJZGMGSQD0SNA5";
-//	public static final String API_SECRET = "WX905C6V8K2NGDC9RBGVPH7NIARQSGUA4VVV746O";
-	
-	public static final String API_BASE_URL = "http://localhost:8080/p-market-api";
+	public static final String API_BASE_URL = "https://api.whatspos.com/p-market-api";
 	public static final String API_KEY = "NGUIWQGJZGMGSQD0SNA5";
 	public static final String API_SECRET = "WX905C6V8K2NGDC9RBGVPH7NIARQSGUA4VVV746O";
-	
 	
 }
