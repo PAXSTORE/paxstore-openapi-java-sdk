acb70c3c160b5c7919489b735893ffb428658721
tanjie <tqjy13@163.com>
2019-07-08 09:21:33 +0800

    rename api method

--- src/main/java/com/pax/market/api/sdk/java/api/terminalFirmware/dto/PushFirmware2TerminalRequest.java
+++ src/main/java/com/pax/market/api/sdk/java/api/terminalFirmware/dto/PushFirmware2TerminalRequest.java
@@ -8,7 +8,37 @@ import java.io.Serializable;
  * @author liukai
  * @date 2019/5/23
  */
-public class PushFirmware2TerminalRequest extends PushFirmwareTaskBasicRequest {
+public class PushFirmware2TerminalRequest implements Serializable {
+
 
     private static final long serialVersionUID = -7281022470088330469L;
+
+    private String tid;
+    private String serialNo;
+    @NotBlank
+    private String fmName;
+
+    public String getTid() {
+        return tid;
+    }
+
+    public void setTid(String tid) {
+        this.tid = tid;
+    }
+
+    public String getSerialNo() {
+        return serialNo;
+    }
+
+    public void setSerialNo(String serialNo) {
+        this.serialNo = serialNo;
+    }
+
+    public String getFmName() {
+        return fmName;
+    }
+
+    public void setFmName(String fmName) {
+        this.fmName = fmName;
+    }
 }
