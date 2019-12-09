8d164917642f1c144f5cfbb1beb5ce79780ed522
tanjie <tqjy13@163.com>
2019-12-09 16:35:38 +0800

    update

--- src/test/java/com/pax/market/api/sdk/java/api/test/PushHistoryApiTest.java
+++ src/test/java/com/pax/market/api/sdk/java/api/test/PushHistoryApiTest.java
@@ -14,20 +14,11 @@ package com.pax.market.api.sdk.java.api.test;
 import com.pax.market.api.sdk.java.api.base.dto.Result;
 import com.pax.market.api.sdk.java.api.pushHistory.PushHistoryApi;
 import com.pax.market.api.sdk.java.api.pushHistory.dto.AppPushHistoryDTO;
-import com.pax.market.api.sdk.java.api.terminalVariable.TerminalVariableApi;
-import com.pax.market.api.sdk.java.api.terminalVariable.dto.ParameterVariable;
-import com.pax.market.api.sdk.java.api.terminalVariable.dto.ParameterVariableDTO;
-import com.pax.market.api.sdk.java.api.terminalVariable.dto.TerminalParameterVariableDeleteRequest;
-import com.pax.market.api.sdk.java.api.terminalVariable.dto.TerminalParameterVariableRequest;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class PushHistoryApiTest {
     private static final Logger logger = LoggerFactory.getLogger(PushHistoryApiTest.class.getSimpleName());
@@ -36,12 +27,12 @@ public class PushHistoryApiTest {
 
     @Before
     public void init(){
-        pushHistoryApi = new PushHistoryApi("http://localhost:8080/p-market-api", "0TAJEJK00R8YYK3HY0LI", "PMJ81GE1V0OCMTE7IFJBKPBE0RU0UGUGIBNJAJ5O");
+        pushHistoryApi = new PushHistoryApi(TestConstants.API_BASE_URL, TestConstants.API_KEY, TestConstants.API_SECRET);
     }
 
     @Test
     public void testGetTerminalVariable(){
-        Result<AppPushHistoryDTO> result = pushHistoryApi.searchAppPushHistory(1, 10, PushHistoryApi.PushHistorySearchOrderBy.AppPushTime, "com.pax.posviewer", "", PushHistoryApi.PushStatus.Success, null);
+        Result<AppPushHistoryDTO> result = pushHistoryApi.searchAppPushHistory(1, 10, PushHistoryApi.PushHistorySearchOrderBy.AppPushTime, "com.pax.posviewer", "", null, null);
         Assert.assertEquals(0, result.getBusinessCode());
     }
 }
