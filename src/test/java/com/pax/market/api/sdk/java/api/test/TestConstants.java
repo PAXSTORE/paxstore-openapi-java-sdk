f781dd67b7b7f4cf42d3b6d171ab2eae5ea8015d
毛正文 <546166705@qq.com>
2024-12-10 17:53:19 +0800

    rename TerminalCPUStatistic API into TerminalSystemUsage, and return RAM information

--- src/test/java/com/pax/market/api/sdk/java/api/test/TestConstants.java
+++ src/test/java/com/pax/market/api/sdk/java/api/test/TestConstants.java
@@ -18,16 +18,8 @@ package com.pax.market.api.sdk.java.api.test;
  */
 public interface TestConstants {
 
-	String API_BASE_URL = "http://www.paxdev.com:8085/p-market-api";
-	//取自pax_market_3rd_sys表
-//	devsit
-	String API_KEY = "DWZY1OPIUJ3YASS2O4DG";
-	String API_SECRET = "FXY28485W5XG3ERAQUQUVYTPX2O0IC1Q4L2OULO3";
-//  sit
-//	String API_KEY="0TAJEJK00R8YYK3HY0LI";
-//	String API_SECRET = "1HPWC6XACCMV5TXURERX927WVAVTQUMGN50DSD75";
-//	徐建使用
-//	String API_KEY="7A6H7XP291VMW13TLD2V";
-//	String API_SECRET = "Z2YS3WN6CL6NY8OGFM3WF6HN0SB8ZPUAENX42BTE";
+	String API_BASE_URL = "https://api.whatspos.com/p-market-api";
+	String API_KEY = "XXXXXXXX";
+	String API_SECRET = "XXXXXXXXXXXXXXXX";
 
 }
